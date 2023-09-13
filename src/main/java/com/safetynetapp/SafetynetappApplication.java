b5fc4b68faee1d29c0ceef1dde4166a4d428fe96
cariocaphil <cariocaphil@gmail.com>
2023-09-13 23:12:31 +0200

    initial commit

---
+++ src/main/java/com/safetynetapp/SafetynetappApplication.java
@@ -0,0 +1,13 @@
+package com.safetynetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetynetappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetynetappApplication.class, args);
+	}
+
+}
