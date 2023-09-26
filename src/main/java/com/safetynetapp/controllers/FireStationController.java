840b5fa68c545076438ab8ede31b09b22d4e8dda
cariocaphil <cariocaphil@gmail.com>
2023-09-26 22:52:03 +0200

    add initial version of FireStation crud functionality

--- src/main/java/com/safetynetapp/controllers/FireStationController.java
+++ src/main/java/com/safetynetapp/controllers/FireStationController.java
@@ -1,15 +1,20 @@
 package com.safetynetapp.controllers;
 
+import com.safetynetapp.models.FireStation;
 import com.safetynetapp.models.FireStationInfoResponse;
-
+import com.safetynetapp.models.FireStationMappingDeleteRequest;
+import com.safetynetapp.models.FireStationMappingUpdateRequest;
 import com.safetynetapp.services.FireStationService;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-
 
 @RestController
 public class FireStationController {
@@ -31,5 +36,37 @@ public class FireStationController {
     }
 
     return ResponseEntity.ok().body(response);
+  }
+
+  @PostMapping("/firestation")
+  public ResponseEntity<String> addFireStation(@RequestBody FireStation fireStation) {
+    boolean success = fireStationService.addFireStation(fireStation);
+    if (success) {
+      return ResponseEntity.ok("Fire station added successfully");
+    } else {
+      return ResponseEntity.badRequest().body("Fire station with the given number already exists");
+    }
+  }
+
+  @PutMapping("/firestation")
+  public ResponseEntity<Object> updateFireStation(
+      @RequestBody FireStationMappingUpdateRequest request) {
+    boolean success = fireStationService.updateFireStation(request);
+    if (success) {
+      return ResponseEntity.ok().body("Fire station mapping updated successfully");
+    } else {
+      return ResponseEntity.badRequest().body("Fire station with the given number already exists");
+    }
+  }
+
+  @DeleteMapping("/firestation")
+  public ResponseEntity<String> deleteFireStationMapping(
+      @RequestBody FireStationMappingDeleteRequest request) {
+    boolean success = fireStationService.deleteFireStationMapping(request);
+    if (success) {
+      return ResponseEntity.ok("Fire station mapping deleted successfully");
+    } else {
+      return new ResponseEntity<>("Fire station or address not found", HttpStatus.NOT_FOUND);
+    }
   }
 }
---
+++ src/main/java/com/safetynetapp/models/FireStationMappingDeleteRequest.java
@@ -0,0 +1,15 @@
+package com.safetynetapp.models;
+
+public class FireStationMappingDeleteRequest {
+
+  private String address;
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+}
