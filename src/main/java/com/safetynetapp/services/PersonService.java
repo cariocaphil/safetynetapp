3c0d0242d72a3d935a27c7cf96872d94e4fd9f3b
cariocaphil <cariocaphil@gmail.com>
2023-09-26 23:42:34 +0200

    add logging for PersonService

--- src/main/java/com/safetynetapp/services/PersonService.java
+++ src/main/java/com/safetynetapp/services/PersonService.java
@@ -3,16 +3,14 @@ package com.safetynetapp.services;
 import com.safetynetapp.models.Person;
 import com.safetynetapp.utilities.DataLoader;
 import com.safetynetapp.utilities.DataUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.tinylog.Logger;
 
 @Service
 public class PersonService {
-
-  // Assuming you have appropriate data access methods (e.g., using repositories)
 
   @Autowired
   private DataLoader dataLoader;
@@ -34,18 +32,26 @@ public class PersonService {
   private List<Person> personList = new ArrayList<>();
 
   public boolean addPerson(Person person) {
+    Logger.info("Adding person: {} {}", person.getFirstName(), person.getLastName());
+
     for (Person existingPerson : personList) {
       if (existingPerson.getFirstName().equals(person.getFirstName()) &&
           existingPerson.getLastName().equals(person.getLastName())) {
+        Logger.info("Person already exists: {} {}", existingPerson.getFirstName(),
+            existingPerson.getLastName());
         return false;
       }
     }
 
     personList.add(person);
+    Logger.info("Added person: {} {}", person.getFirstName(), person.getLastName());
     return true;
   }
 
   public boolean updatePerson(Person updatedPerson) {
+    Logger.info("Updating person: {} {}", updatedPerson.getFirstName(),
+        updatedPerson.getLastName());
+
     for (Person existingPerson : personList) {
       if (existingPerson.getFirstName().equals(updatedPerson.getFirstName()) &&
           existingPerson.getLastName().equals(updatedPerson.getLastName())) {
@@ -55,21 +61,31 @@ public class PersonService {
         existingPerson.setPhone(updatedPerson.getPhone());
         existingPerson.setEmail(updatedPerson.getEmail());
 
+        Logger.info("Updated person: {} {}, Address: {}, City: {}, Zip: {}, Phone: {}, Email: {}",
+            existingPerson.getFirstName(), existingPerson.getLastName(),
+            existingPerson.getAddress(), existingPerson.getCity(),
+            existingPerson.getZip(), existingPerson.getPhone(), existingPerson.getEmail());
         return true;
       }
     }
 
+    Logger.info("No person found for updating: {} {}", updatedPerson.getFirstName(),
+        updatedPerson.getLastName());
     return false;
   }
 
   public boolean deletePerson(String firstName, String lastName) {
+    Logger.info("Deleting person: {} {}", firstName, lastName);
+
     for (Person person : personList) {
       if (person.getFirstName().equals(firstName) && person.getLastName().equals(lastName)) {
         personList.remove(person);
+        Logger.info("Deleted person: {} {}", person.getFirstName(), person.getLastName());
         return true;
       }
     }
 
+    Logger.info("No person found for deletion: {} {}", firstName, lastName);
     return false;
   }
 }
