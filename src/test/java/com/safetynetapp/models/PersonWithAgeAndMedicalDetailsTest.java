f3e4f501bf218c1efd9d0b2573c7ba300ec8efc5
cariocaphil <cariocaphil@gmail.com>
2023-11-03 20:56:58 +0100

    add test for PersonWithAgeAndMedicalDetailsTest

--- src/test/java/com/safetynetapp/models/PersonWithAgeAndMedicalDetailsTest.java
+++ src/test/java/com/safetynetapp/models/PersonWithAgeAndMedicalDetailsTest.java
@@ -37,4 +37,36 @@ class PersonWithAgeAndMedicalDetailsTest {
     // Act & Assert
     assertEquals(person1.hashCode(), person2.hashCode()); // Expecting hash codes to be equal
   }
+
+  @Test
+  void testGettersAndSetters() {
+    // Arrange
+    List<String> medications = Arrays.asList("Medication1", "Medication2");
+    List<String> allergies = Arrays.asList("Allergy1", "Allergy2");
+
+    PersonWithAgeAndMedicalDetails person = new PersonWithAgeAndMedicalDetails("John", "Doe", "555-555-5555", 30, medications, allergies);
+
+    // Act & Assert
+    assertEquals("John", person.getFirstName());
+    assertEquals("Doe", person.getLastName());
+    assertEquals("555-555-5555", person.getPhone());
+    assertEquals(30, person.getAge());
+    assertEquals(medications, person.getMedications());
+    assertEquals(allergies, person.getAllergies());
+
+    // Test setters
+    person.setFirstName("Jane");
+    person.setLastName("Smith");
+    person.setPhone("123-456-7890");
+    person.setAge(40);
+    person.setMedications(Arrays.asList("NewMedication"));
+    person.setAllergies(Arrays.asList("NewAllergy"));
+
+    assertEquals("Jane", person.getFirstName());
+    assertEquals("Smith", person.getLastName());
+    assertEquals("123-456-7890", person.getPhone());
+    assertEquals(40, person.getAge());
+    assertEquals(Arrays.asList("NewMedication"), person.getMedications());
+    assertEquals(Arrays.asList("NewAllergy"), person.getAllergies());
+  }
 }
